4f1d6c2cf0486f6e17f0b2ac7b07dda022c25f8f
jdkfx <amateurranch@gmail.com>
2024-09-01 21:45:12 +0900

    :package: genpdfを追加し、PDFファイルの出力をする

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Exporting PDF files...");
+
+    let font_family = genpdf::fonts::from_files("./fonts/Noto_Sans/static/", "NotoSans", None)
+        .expect("Failed to load font family");
+    let mut doc = genpdf::Document::new(font_family);
+    doc.set_title("Demo document");
+    let mut decorator = genpdf::SimplePageDecorator::new();
+    decorator.set_margins(10);
+    doc.set_page_decorator(decorator);
+    doc.push(genpdf::elements::Paragraph::new("This is a demo document."));
+    doc.render_to_file("output.pdf").expect("Failed to write PDF file");
 }
