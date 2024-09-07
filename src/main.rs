36d10f9d8fb008452a244ce12c3427626ca39447
jdkfx <amateurranch@gmail.com>
2024-09-08 00:17:53 +0900

    :sparkles: 字句解析を行い、トークン列の作成を行う

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,42 @@ use genpdf::Element;
 use genpdf::{Document, SimplePageDecorator, Size, Mm, fonts, style::Style};
 use genpdf::elements::Paragraph;
 
+#[derive(Debug, PartialEq)]
+enum State {
+    Data,
+    TagOpen,
+    EndTagOpen,
+    TagName,
+    SelfClosingStartTag,
+}
+
+#[derive(Debug, Clone)]
+enum TokenType {
+    StartTag,
+    EndTag,
+    Char,
+    Eof,
+}
+
+#[derive(Debug, Clone)]
+struct Token {
+    token_type: TokenType,
+    tag_name: Option<String>,
+    self_closing: bool,
+    data: Option<String>,
+}
+
+impl Token {
+    fn new(token_type: TokenType, tag_name: Option<String>, self_closing: bool, data: Option<String>) -> Self {
+        Token {
+            token_type,
+            tag_name,
+            self_closing,
+            data,
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -48,4 +84,79 @@ fn main() {
 
     println!("Exporting PDF files...");
     doc.render_to_file(output_file_name).expect("Failed to write PDF file");
+
+    show_xml_tokens();
+}
+
+fn tokenize(xml: &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    let mut state = State::Data;
+    let mut current_token: Option<Token> = None;
+
+    let mut chars = xml.chars().peekable();
+    while let Some(c) = chars.next() {
+        match state {
+            State::Data => {
+                if c == '<' {
+                    state = State::TagOpen;
+                } else {
+                    tokens.push(Token::new(TokenType::Char, None, false, Some(c.to_string())));
+                }
+            }
+            State::TagOpen => {
+                if c == '/' {
+                    state = State::EndTagOpen;
+                } else if c.is_alphabetic() {
+                    current_token = Some(Token::new(TokenType::StartTag, Some(c.to_string()), false, None));
+                    state = State::TagName;
+                } else {
+                    tokens.push(Token::new(TokenType::Char, None, false, Some('<'.to_string())));
+                    state = State::Data;
+                }
+            }
+            State::EndTagOpen => {
+                if c.is_alphabetic() {
+                    current_token = Some(Token::new(TokenType::EndTag, Some(c.to_string()), false, None));
+                    state = State::TagName;
+                }
+            }
+            State::TagName => {
+                if c == '/' {
+                    state = State::SelfClosingStartTag;
+                } else if c == '>' {
+                    if let Some(token) = current_token.take() {
+                        tokens.push(token);
+                    }
+                    state = State::Data;
+                } else if c.is_uppercase() {
+                    if let Some(ref mut token) = current_token {
+                        token.tag_name = Some(token.tag_name.clone().unwrap() + &c.to_lowercase().to_string());
+                    }
+                } else if let Some(ref mut token) = current_token {
+                    token.tag_name = Some(token.tag_name.clone().unwrap() + &c.to_string());
+                }
+            }
+            State::SelfClosingStartTag => {
+                if c == '>' {
+                    if let Some(ref mut token) = current_token {
+                        token.self_closing = true;
+                        tokens.push(token.clone());
+                    }
+                    state = State::Data;
+                }
+            }
+        }
+    }
+
+    tokens.push(Token::new(TokenType::Eof, None, false, None));
+    tokens
+}
+
+fn show_xml_tokens() {
+    let xml = "<deck><title>Sample Deck</title></deck>";
+    let tokens = tokenize(xml);
+
+    for token in tokens {
+        println!("{:?}", token);
+    }
 }
